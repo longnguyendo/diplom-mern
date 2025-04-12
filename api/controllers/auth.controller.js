c2c64fa21665614915ccdca010a0f4d371ce30f2
longnguyendo <longminhon@LongMinhon.>
2025-04-12 14:12:43 +0300

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
+import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     console.log(req.body);
@@ -11,7 +12,7 @@ export const signup = async (req, res, next) => {
         next(errorHandler(400, 'All fields are required'));
     }
 
-    const hashedPassword = bcrypt.hashSync(password, 15);
+    const hashedPassword = bcryptjs.hashSync(password, 15);
     // in es6 key familiar value => we use the same word
     // no need to assign 
     const newUser = new User({
@@ -26,5 +27,37 @@ export const signup = async (req, res, next) => {
     }
     catch (err) {
         next(err);
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password } = req.body;
+    if ( !email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({email});
+        console.log(validUser);
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'));
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            next(errorHandler(400, 'Invalid password'));
+        }
+        const token = jwt.sign(
+            {id: validUser._id}, 
+            process.env.JWT_SECRET,
+        )
+
+        const { password: _pass, ...rest} = validUser._doc;
+
+        res.status(200).cookie('access_token', token , {
+            httpOnly: true,
+        }).json(rest);
+        // res.json({token})
+    } catch(error) {
+        next(error);
     }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { signup, signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.get('/test', (req, res) => {
 });
 
 router.post('/signup', signup);
+router.post('/signin', signin);
 
 export default router;
