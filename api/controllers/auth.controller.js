a8ad15ab0d98ae2bdf33dee29b08f6e4fe6f1e74
longnguyendo <longminhon@LongMinhon.>
2025-04-12 17:48:57 +0300

    accidentally added redux in global node_module, it only neccessary for fe

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -38,25 +38,27 @@ export const signin = async (req, res, next) => {
 
     try {
         const validUser = await User.findOne({email});
-        console.log(validUser);
+        // console.log(validUser);
         if (!validUser) {
             return next(errorHandler(404, 'User not found'));
         }
         const validPassword = bcryptjs.compareSync(password, validUser.password);
         if (!validPassword) {
-            next(errorHandler(400, 'Invalid password'));
+            return next(errorHandler(400, 'Invalid password'));
         }
         const token = jwt.sign(
             {id: validUser._id}, 
             process.env.JWT_SECRET,
         )
 
-        const { password: _pass, ...rest} = validUser._doc;
+        const { password: pass, ...rest} = validUser._doc;
 
-        res.status(200).cookie('access_token', token , {
+        res
+        .status(200)
+        .cookie('access_token', token , {
             httpOnly: true,
-        }).json(rest);
-        // res.json({token})
+        })
+        .json(rest);
     } catch(error) {
         next(error);
     }
