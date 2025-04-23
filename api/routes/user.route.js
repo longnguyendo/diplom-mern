df2108af748b25b69f1d1c820dd538c601aba8df
Long Be <longnguyen.open@gmail.com>
2025-04-24 00:19:35 +0300

    test username

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser, deleteUser, signoutUser } from '../controllers/user.controller.js';
+import { test, updateUser, deleteUser, signoutUser, getUsers, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -12,5 +12,7 @@ router.get('/update', (req, res) => {
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signoutUser);
+router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
