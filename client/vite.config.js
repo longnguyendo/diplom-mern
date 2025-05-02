7e6d1d588f37f50984da5ef18de772d7c31e421b
Long Be <longnguyen.open@gmail.com>
2025-05-02 23:36:56 +0300

    use link db normally not use env

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import flowbiteReact from "flowbite-react/plugin/vite";
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL,
+        target: 'https://diplom-mern-blog.onrender.com',
         changeOrigin: true,
         rewrite: (path) => { return path }
       }
