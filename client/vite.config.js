2cce63301c2eda1249b2c187c3ca381c95a820f6
Long Be <longnguyen.open@gmail.com>
2025-05-02 15:53:33 +0300

    add link target in vite config js and alias

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import flowbiteReact from "flowbite-react/plugin/vite";
 
+
 // https://vite.dev/config/
 export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://diplom-mern-blog.onrender.com',
         secure: false,
       }
     }
   },
   plugins: [react(), flowbiteReact()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
